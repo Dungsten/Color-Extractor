21d40b35272fabd02e77c9b6a386393da0a3d67d
RitikBansal388 <ritik.bansal388@gmail.com>
2021-07-24 01:47:38 +0530

    checking

--- color-extractor/src/App.js
+++ color-extractor/src/App.js
@@ -1,12 +1,7 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+  return <div className="App">hi Sagar</div>;
 }
 
 export default App;
